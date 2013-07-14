37756e4065f444b29c8e294585df499534c39dd0
Swapnil Dukhande <libranswapy@gmail.com>
2013-07-14 03:09:16 -0500

    Update web.js

--- web.js
+++ web.js
@@ -1,12 +1,32 @@
+
 var express = require('express');
+var fs = require('fs');
+
+ var input = fs.readFileSync('index.html', 'utf-8');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(input);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
